9a973ccacd34bf60d5faf0255673811a54766151
himangshu73 <himangshu73@gmail.com>
2024-05-29 08:04:08 +0600

    unit, auto date, alert added

--- src/Items/Items.jsx
+++ src/Items/Items.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 function Items() {
   const [itemName, setItemName] = useState("");
   const [itemQuantity, setItemQuantity] = useState();
+  const [itemUnit, setItemUnit] = useState("");
   const [itemPrice, setItemPrice] = useState();
   const [items, setItems] = useState([]);
 
@@ -15,17 +16,29 @@ function Items() {
   const handlePriceChange = (event) => {
     setItemPrice(event.target.value);
   };
+  const handleUnitChange = (event) => {
+    setItemUnit(event.target.value);
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
+
+    if (!itemName || !itemQuantity || !itemUnit || !itemPrice) {
+      alert("Please fill all fields");
+      return;
+    }
+
     const newItem = {
       name: itemName,
       quantity: parseInt(itemQuantity),
+      unit: itemUnit,
       price: parseInt(itemPrice),
+      date: new Date().toLocaleDateString(),
     };
     setItems([...items, newItem]);
     setItemName("");
     setItemQuantity("");
+    setItemUnit("");
     setItemPrice("");
   };
 
@@ -42,6 +55,7 @@ function Items() {
           onChange={handleNameChange}
           name="item"
         />
+        <br />
         <label htmlFor="quantity">Quantity</label>
         <input
           type="number"
@@ -50,6 +64,36 @@ function Items() {
           onChange={handleQuantityChange}
           name="quantity"
         />
+        <label htmlFor="">
+          <input
+            type="radio"
+            value="KG"
+            checked={itemUnit === "KG"}
+            onChange={handleUnitChange}
+          />
+          KG
+        </label>
+
+        <label htmlFor="">
+          <input
+            type="radio"
+            value="Ltr"
+            checked={itemUnit === "Ltr"}
+            onChange={handleUnitChange}
+          />
+          Ltr
+        </label>
+
+        <label htmlFor="">
+          <input
+            type="radio"
+            value="Piece"
+            checked={itemUnit === "Piece"}
+            onChange={handleUnitChange}
+          />
+          Piece
+        </label>
+        <br />
         <label htmlFor="price">Price</label>
         <input
           type="number"
@@ -58,13 +102,15 @@ function Items() {
           onChange={handlePriceChange}
           name="price"
         />
+        <br />
         <button type="submit">Submit</button>
       </form>
       <h2>Submitted Text Here</h2>
       <ul>
         {items.map((item, index) => (
           <li key={index}>
-            Name: {item.name}, Quantity:{item.quantity}, Price: {item.price}
+            Name: {item.name}, Quantity:{item.quantity}
+            {item.unit}, Price: {item.price}, Date:{item.date}
           </li>
         ))}
       </ul>
